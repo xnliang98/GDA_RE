bb5c12d5b1af5c7e9d8d76a4789cc02f9dccf8a0
xnliang <xnliang19@163.com>
2019-11-14 16:45:27 +0800

    add shell script

---
+++ cgcn.sh
@@ -0,0 +1,7 @@
+if [ ! -d "dataset/vocab" ]; then
+  python prepare_vocab.py dataset/tacred dataset/vocab
+fi
+
+python train_cgcn.py --id 00 --conv_l2 0.002 --pooling_l2 0.002 --info "CGCN model" 
+
+python eval.py > res.log
